14f8ce2f6fc7525df3578ed6ff81f6aa15d77e33
依旧 <1127740739@qq.com>
2022-09-24 18:20:51 +0800

    dev file and prod file

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,11 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+  devServer: {
+    host: 'localhost',
+    port: 8090,
+    open: true
+  }
+}
